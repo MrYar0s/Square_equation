0a27b4af53313da08e38839710d77243f182781b
MrYar0s <70350421+MrYar0s@users.noreply.github.com>
2020-08-29 19:38:57 +0300

    Delete main_8c.js

--- html/main_8c.js
+++
@@ -1,9 +0,0 @@
-var main_8c =
-[
-    [ "BORDER", "main_8c.html#a6d0652ae6ea6a5c4fef68baf139fd085", null ],
-    [ "INF_ROOTS", "main_8c.html#a80c90036cc246446050cb1c01bf254e7", null ],
-    [ "Compare", "main_8c.html#a49cbda4f873105c4e69728dec420ecbe", null ],
-    [ "main", "main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ],
-    [ "SolveEquation", "main_8c.html#ac4027722f6b7429508c3f453af3e4c45", null ],
-    [ "unit_test", "main_8c.html#a8e55f3e436b0268b26875038536f8e53", null ]
-];
